0f0d31ed0f58638a40e3d26b1b36a33fd0354cca
V-k1ng <153673049+V-k1ng@users.noreply.github.com>
2024-03-18 23:14:35 +0300

    Delete Runable.java

--- Runable.java
+++
@@ -1,8 +0,0 @@
-public class Runable implements Runnable {
-
-    @Override
-    public void run() {
-        long threadId = Thread.currentThread().getId();
-        System.out.println("Thread ID: " + threadId);
-    }
-}
